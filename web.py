ccd64e42ba5d984c92e63daa33f758acb3d3b8b3
TeinerHD <107218266+TeinerHD@users.noreply.github.com>
2023-07-04 11:02:07 +0200

    Update web.py
    yaay

--- web.py
+++ web.py
@@ -3,6 +3,18 @@ from flask import send_from_directory
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1*'
+app.config['MYSQL_DATABASE_DB'] = 'inovatek'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor =conn.cursor()
+
+cursor.execute("SELECT position.id, poslat, poslon, remaining_battery, type from position LEFT JOIN battery ON position.id = battery.id LEFT JOIN model ON battery.id = model.id")
+data = cursor.fetchone()
 
 @app.route("/")
 def home():
