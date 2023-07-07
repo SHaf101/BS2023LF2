47600c479480e86bedf650ea763da7e29697476b
christian <christian05.studt@outlook.de>
2023-07-07 10:16:12 +0200

    new file:   web finished.py 	deleted:    web copy.py

---
+++ web finished.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1*'
+app.config['MYSQL_DATABASE_DB'] = 'inovatek'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor =conn.cursor()
+
+cursor.execute("SELECT poslon, poslat, position.id, remaining_battery from position LEFT JOIN battery ON position.id = battery.id LEFT JOIN model ON battery.id = model.id")
+data = cursor.fetchall()
+
+@app.route("/")
+def home():
+	return render_template("webapp.html", items=data)
+
+if __name__ == "__main__":
+	app.run()
